3ad1efe9ead0f29289c8b4c5ea6383c86629bbfd
CJM1994 <connorjohnmullin@gmail.com>
2022-05-01 13:31:24 -0700

    add picture options, will add pic w/ transparent background this week

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,31 +1,31 @@
-import CTA from './CTA';
-import HeaderSocials from './HeaderSocials';
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
 
-import ME from '../../assets/me.png';
-import './Header.css';
+import ME from "../../assets/me.jpg";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header>
-
-      <div className='container header__container'>
+      <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Connor Mullin</h1>
         <h5 className="text-light">Full-Stack Developer</h5>
+
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll down
+        </a>
+
+        <HeaderSocials />
       </div>
-
-      <CTA />
-
-      <div className="me" >
-        <img src={ME} alt='me' />
-      </div>
-
-      <a href='#contact' className="scroll__down">Scroll down</a>
-
-      <HeaderSocials />
-
     </header>
-  )
-}
+  );
+};
 
 export default Header;

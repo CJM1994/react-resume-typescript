d265899a1d89c9ca1578c6d965e788da2d3b777e
CJM1994 <connorjohnmullin@gmail.com>
2022-05-02 16:53:44 -0700

    add real content to portfolio section

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -2,11 +2,11 @@ import './Portfolio.css';
 
 // Portfolio Images
 import IMG1 from '../../assets/code-typr.png';
-import IMG2 from '../../assets/code-typr.png';
-import IMG3 from '../../assets/code-typr.png';
-import IMG4 from '../../assets/code-typr.png';
-import IMG5 from '../../assets/code-typr.png';
-import IMG6 from '../../assets/code-typr.png';
+import IMG2 from '../../assets/smart-to-do.gif';
+import IMG3 from '../../assets/scheduler.png';
+import IMG4 from '../../assets/tweeter.png';
+import IMG5 from '../../assets/etch-a-sketch.png';
+import IMG6 from '../../assets/portfolio-site.png';
 
 const portfolioData = [
   {
@@ -19,37 +19,37 @@ const portfolioData = [
   {
     id: 2,
     image: IMG2,
-    title: 'Code-Typr',
-    github: 'https://github.com/CJM1994/react-resume-typescript',
-    live: 'https://code-typr.netlify.app/',
+    title: 'Smart-ToDo',
+    github: 'https://github.com/CJM1994/Smart_ToDo_App',
+    live: null,
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Code-Typr',
-    github: 'https://github.com/CJM1994/react-resume-typescript',
-    live: 'https://code-typr.netlify.app/',
+    title: 'Scheduler',
+    github: 'https://github.com/CJM1994/scheduler',
+    live: 'https://schedulerlhlapp.netlify.app/',
   },
   {
     id: 4,
     image: IMG4,
-    title: 'Code-Typr',
-    github: 'https://github.com/CJM1994/react-resume-typescript',
-    live: 'https://code-typr.netlify.app/',
+    title: 'Tweeter',
+    github: 'https://github.com/CJM1994/tweeter/',
+    live: 'https://tweeter-responsive-design.herokuapp.com/',
   },
   {
     id: 5,
     image: IMG5,
-    title: 'Code-Typr',
-    github: 'https://github.com/CJM1994/react-resume-typescript',
-    live: 'https://code-typr.netlify.app/',
+    title: 'Etch-A-Sketch',
+    github: 'https://github.com/CJM1994/Project-Etch-a-Sketch/',
+    live: 'https://cjm1994.github.io/Project-Etch-a-Sketch/',
   },
   {
     id: 6,
     image: IMG6,
-    title: 'Code-Typr',
+    title: 'Portfolio Site',
     github: 'https://github.com/CJM1994/react-resume-typescript',
-    live: 'https://code-typr.netlify.app/',
+    live: null,
   },
 ];
 
@@ -71,7 +71,7 @@ const Portfolio = () => {
                 <h3>{title}</h3>
                 <div className='portfolio__item--cta'>
                   <a href={github} target='_blank' className='btn'>GitHub</a>
-                  <a href={live} target='_blank' className='btn btn-primary'>Live Demo</a>
+                  {live !== null && <a href={live} target='_blank' className='btn btn-primary'>Live Demo</a>}
                 </div>
               </article>
             );

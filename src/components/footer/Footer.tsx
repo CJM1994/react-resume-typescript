7bd213c9eb38ce61963d8dda6247b935a0ad6647
CJM1994 <connorjohnmullin@gmail.com>
2022-05-03 13:28:31 -0700

    Add footer content and styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import {BsLinkedin, BsGithub, BsStackOverflow} from 'react-icons/bs';
 import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">CONNOR</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/connorjmullin/" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/CJM1994" target="_blank"><BsGithub /></a>
+        <a href='https://stackoverflow.com/users/17381940/connor-mullin' target='_blank'><BsStackOverflow /></a>
+      </div>
+    </footer>
   )
 }
 
